9b393a91739720188fd3f1e916136100d51ecd29
BabuMendes <125094466+BabuMendes@users.noreply.github.com>
2023-02-17 18:40:54 -0300

    Implementação de escadas
    Eu criei uma escada no dia 14 de fevereiro a noite no curso de programação de jogos

--- Assets/Scripts/Corpo.cs
+++ Assets/Scripts/Corpo.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Corpo : MonoBehaviour
 {
-    private Rigidbody Rb; 
+    private Rigidbody Rb;
+    private Animator Anim;
     // Start is called before the first frame update
     void Start()
     {
         Rb = GetComponent<Rigidbody>();
+        Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,5 +26,14 @@ public class Corpo : MonoBehaviour
         float velZ = Input.GetAxis("Vertical");
         float velX = Input.GetAxis("Horizontal");
         Rb.velocity = new Vector3(velX, Rb.velocity.y, velZ);
+
+        if(velX != 0 || velZ !=0)
+        {
+            Anim.SetBool("Andar", true);
+        }
+        else if(velX == 0 && velZ == 0)
+        {
+            Anim.SetBool("Andar", false);
+        }
     }
 }
